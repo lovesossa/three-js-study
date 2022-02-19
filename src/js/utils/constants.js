8338e23ae7af7e807f1b914b598d98a6f2c50067
lovesossa <qwertyerku5@gmail.com>
2022-02-19 19:57:11 +0200

    build version

--- src/js/utils/constants.js
+++
@@ -1,8 +0,0 @@
-export const GLOBAL_VARS = {
-	degree: 0.0174532925,
-	mediaPoint1: 1024,
-	mediaPoint2: 768,
-	mediaPoint3: 480,
-	mediaPoint4: 320,
-	projectDevStatus: process.env.NODE_ENV === 'development',
-};
