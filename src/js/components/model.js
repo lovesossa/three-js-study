99431544bcd92b0a0ce23be8556b1a219e0c6cbc
geoshavel <geoshavel@gmail.com>
2022-04-07 11:47:10 +0300

    fixes

--- src/js/components/model.js
+++ src/js/components/model.js
@@ -1,24 +1,37 @@
+/* eslint-disable no-undef */
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import gsap from 'gsap';
+import Observer from 'gsap/Observer';
 
 function model() {
+	gsap.registerPlugin(Observer, ScrollSmoother, ScrollTrigger);
+
+	// ScrollSmoother.create({
+	// 	wrapper: '.scroll_track',
+	// 	content: '.scroll_track_content',
+	// });
+
+	//---------------------------------------------
+
 	let scene;
 	let camera;
 	let renderer;
 	let container = document.querySelector('.canvas');
 
-	let rotationVal = 10;
-	let rotationStep = 0.15;
+	const objPositionZ = -1.56;
 
-	renderer = new THREE.WebGLRenderer({ antialias: true });
+	renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 	renderer.setSize(window.innerWidth, window.innerHeight);
 
 	const canvas = renderer.domElement;
 	container.appendChild(canvas);
 
+	renderer.setClearColor(0x000000, 0); // the default
+
 	scene = new THREE.Scene();
-	scene.background = new THREE.Color(0xffffff);
+	// scene.background = new THREE.Color(0xffffff);
 
 	// ---------------------------------------------View settings
 	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 3000);
@@ -26,7 +39,7 @@ function model() {
 	camera.position.y = 50;
 	camera.position.x = 0;
 
-	scene.rotation.y = rotationVal;
+	scene.rotation.y = objPositionZ;
 
 	const controls = new OrbitControls(camera, canvas);
 	controls.enableZoom = false;
@@ -70,14 +83,20 @@ function model() {
 	// ---------------------------------------------Load model###
 
 	// ---------------------------------------------Handlers
-	function onHandleScroll(e) {
-		if (e.deltaY < 0) {
-			rotationVal += rotationStep;
-		}
-		if (e.deltaY > 0) {
-			rotationVal -= rotationStep;
-		}
-		scene.rotation.y = rotationVal.toFixed(2);
+	function objectRotateAnim(dir) {
+		const step = 0.12;
+		const transitionStep = 0.0018;
+		const interval = 15;
+		let transition = 0;
+
+		const anim = setInterval(() => {
+			if (transition < step) {
+				transition += transitionStep;
+				scene.rotation.y += transition * dir;
+			} else {
+				clearInterval(anim);
+			}
+		}, interval);
 	}
 
 	function onWindowResize() {
@@ -89,7 +108,7 @@ function model() {
 	// ---------------------------------------------Handlers###
 
 	// ---------------------------------------------Events
-	window.addEventListener('wheel', onHandleScroll, false);
+	// window.addEventListener('wheel', onHandleScroll, false);
 	window.addEventListener('resize', onWindowResize, false);
 	// ---------------------------------------------Events###
 
@@ -101,6 +120,126 @@ function model() {
 	}
 	animate();
 	// ---------------------------------------------Render###
+
+	const sections = document.querySelectorAll('.heading');
+	const images = document.querySelectorAll('.heading_bg');
+	const headings = gsap.utils.toArray('.heading_title');
+	const outerWrappers = gsap.utils.toArray('.heading_out');
+	const innerWrappers = gsap.utils.toArray('.heading_in');
+	const splitHeadings = headings.map((heading) => new SplitText(heading, {
+		type: 'chars,words,lines',
+		linesClass: 'clip-text',
+	}));
+	const wrap = gsap.utils.wrap(0, sections.length - 1);
+	let currentIndex = -1;
+	let animating;
+
+	gsap.set(outerWrappers, { yPercent: 100 });
+	gsap.set(innerWrappers, { yPercent: -100 });
+
+	function gotoSection(i, direction) {
+		let index = wrap(i); // make sure it's valid
+		animating = true;
+		const fromTop = direction === -1;
+		const dFactor = fromTop ? -1 : 1;
+		const tl = gsap.timeline({
+			defaults: {
+				duration: 1.25,
+				ease: 'power1.inOut',
+			},
+			onComplete: () => {
+				animating = false;
+			},
+		});
+		if (currentIndex >= 0) { // The first time this function runs, current is -1
+			gsap.set(sections[currentIndex], {
+				zIndex: 0,
+			});
+			tl
+				.to(images[currentIndex], {
+					yPercent: -15 * dFactor,
+				})
+				.set(sections[currentIndex], {
+					autoAlpha: 0,
+				});
+		}
+		gsap.set(sections[index], {
+			autoAlpha: 1,
+			zIndex: 1,
+		});
+		tl
+			.fromTo([
+				outerWrappers[index],
+				innerWrappers[index],
+			], {
+				yPercent: (j) => {
+					return j ? (-100 * dFactor) : (100 * dFactor);
+				},
+			}, {
+				yPercent: 0,
+			}, 0)
+			.fromTo(images[index], { yPercent: 15 * dFactor }, { yPercent: 0 }, 0)
+			.fromTo(splitHeadings[index].chars, {
+				autoAlpha: 0,
+				yPercent: 150 * dFactor,
+			}, {
+				autoAlpha: 1,
+				yPercent: 0,
+				duration: 1,
+				ease: 'power2',
+				stagger: {
+					each: 0.05,
+				},
+			}, 0.2);
+
+		currentIndex = index;
+	}
+
+	const $model = document.querySelector('.model');
+	const tl = gsap.timeline({
+		duration: 2,
+		ease: 'power2',
+	});
+
+	Observer.create({
+		target: window,
+		type: 'wheel, touch, pointer, scroll',
+		wheelSpeed: -1,
+		onDown: () => {
+			if (!animating) {
+				gotoSection(currentIndex - 1, -1);
+				objectRotateAnim(-1);
+				tl
+					.to($model, {
+						yPercent: 10,
+						duration: 0.8,
+					})
+					.to($model, {
+						yPercent: 0,
+						duration: 0.6,
+					});
+			}
+		},
+		onUp: (e) => {
+			if (!animating) {
+				gotoSection(currentIndex + 1, 1);
+				objectRotateAnim(1);
+				tl
+					.to($model, {
+						yPercent: -10,
+						duration: 0.8,
+					})
+					.to($model, {
+						yPercent: 0,
+						duration: 0.6,
+					});
+			}
+		},
+		tolerance: 10,
+		preventDefault: true,
+	});
+
+	gotoSection(0, 1);
 }
 
 export default model;
